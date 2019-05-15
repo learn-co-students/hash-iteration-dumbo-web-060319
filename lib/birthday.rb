da3b04fba00ad7800924f782e6bf55e8bc2148f4
Alexander Schelchere <ascellie@gmail.com>
2019-05-15 11:04:56 -0400

    Birthday Bonus - complete

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,3 +11,13 @@ def happy_birthday(birthday_kids)
 end
 
 
+# BONUS
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    message1 = "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    message2 = "You are too old for this."
+    age <= 12 ? shout = message1 : shout = message2 
+    puts shout
+  end
+end
